64f327e2243818fd40ac8c3856ca0c81f55f68cd
UDomingos <49692053+UDomingos@users.noreply.github.com>
2021-12-14 14:46:38 -0300

    Add files via upload

---
+++ projeto.html/assets/js/artistas.js
@@ -0,0 +1,21 @@
+const section = document.querySelector ("section") ;
+const button = document.querySelector("#meu-submit");
+
+button.addEventListener("click",function(){
+  
+})
+async function fetchData ( )  {
+  const response = await fetch("http://localhost:8080/artistas") ;//modificar aplicação para servidor local
+  const data = await response.json();
+
+ data .forEach( (artistas) =>{
+   const content = `<div class = "card">
+       <img src= "${artistas.imagem}"alt="${artistas.nome}">
+       <h2>${ artistas.nome} </h2>
+       <p.${artistas.descricao} </p>
+    </div> ` ;
+section.innerHTML +=content;
+  }) ;
+} 
+
+fetchData ( ) ;
