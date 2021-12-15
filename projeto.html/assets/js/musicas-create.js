6a0f8fc895c44a2709b0cf8fc9a492e574c7dc69
UDomingos <49692053+UDomingos@users.noreply.github.com>
2021-12-15 16:24:58 -0300

    Update musicas-create.js

--- projeto.html/assets/js/musicas-create.js
+++ projeto.html/assets/js/musicas-create.js
@@ -17,7 +17,7 @@ alert("Erro ao cadastrar musica!");
 });
 async function getartistas(){
     const response = await fetch(
-        "https://meu-projeto-api.herokuapp.com/musicas"
+        "http://meu-projeto-api.herokuapp.com/musicas"
     );
     const artistas = await response.json();
     artistas.forEach(element => {
